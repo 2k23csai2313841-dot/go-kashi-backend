2f898d67989e02a94e2feed8c8f07fe797e29eb4
2k23csai2313841-dot <2k23.csai2313841@gmail.com>
2026-04-21 16:27:33 +0530

    Update startRouter.js

--- router/startRouter.js
+++ router/startRouter.js
@@ -4,6 +4,9 @@ const startRouter = express.Router();
 startRouter.get("/",(req,res,next)=>{
   res.send("server is running");
 })
+startRouter.get("/KashiRoute/check",(req, res, next) => {
+    return res.json({ wakeup: true });
 
+});
 
 module.exports = startRouter;
